57e4350a645789c86727a246be9adcc6972a6aa1
zjx182021 <155349472+zjx182021@users.noreply.github.com>
2024-09-08 12:03:03 +0800

    Update api_login.h

--- api/api_login.h
+++ api/api_login.h
@@ -3,6 +3,9 @@
 #include "api_common.h"
 #include "string"
 using namespace std;
-
-int ApiUserLogin(string&url,string & post_data, string & str_json);
+#if API_LOGIN_MUTIL_THREAD  // 该把这个宏定义放到cmakelists.txt才对
+int ApiUserLogin(u_int32_t conn_uuid, std::string &url, std::string &post_data);
+#else
+int ApiUserLogin(string &url, string &post_data, string &str_json);
 #endif
+#endif // ! _API_LOGIN_H_
