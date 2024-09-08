ca363e5e7c86d214e0b3f084943a30fe092b842a
zjx182021 <155349472+zjx182021@users.noreply.github.com>
2024-09-08 12:02:45 +0800

    Update api_myfiles.h

--- api/api_myfiles.h
+++ api/api_myfiles.h
@@ -1,7 +1,9 @@
 #ifndef _API_MYFILES_H_
 #define _API_MYFILES_H_
 #include "api_common.h"
- 
+#if API_MYFILES_MUTIL_THREAD
+int ApiMyfiles(uint32_t conn_uuid, std::string &url, std::string &post_data);
+#else
 int ApiMyfiles(string &url, string &post_data, string &str_json);
 #endif
-
+#endif
